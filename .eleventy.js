121bb8eeca75fb7f237d53e31b336dd6f3a76efc
Abhisek <abhisek.dutt@gmail.com>
2020-07-06 21:35:11 +0530

    Starting to integrate SSG with eleventy

---
+++ .eleventy.js
@@ -0,0 +1,9 @@
+module.exports = {
+    dir: {
+        input: "src",
+        output: "public",
+        includes: "_includes",
+        layouts: "_layouts",
+        htmlTemplateEngine: "ejs"
+    },
+};
